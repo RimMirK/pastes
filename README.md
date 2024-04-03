c6f716337b86131bea8410be3be11d6e28d75a61
RimMirK <115800822+RimMirK@users.noreply.github.com>
2024-04-03 22:37:00 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ pip install pastes
 ```
 
 ## Usage
+### Sync
 ```py
 from pastes import paste
 
@@ -24,6 +25,29 @@ fib(1000)
 url = paste(code)
 
 print(url) # https://pastes.dev/UUHlliP7SF
+```
+
+### Async
+```py
+from pastes import apaste
+import asyncio
+
+code = """
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+fib(1000)
+"""
+
+async def main():
+    url = await apaste(code)
+
+    print(url) # https://pastes.dev/UUHlliP7SF
+
+asyncio.run(main())
 ```
 
 ## Developer
--- __init__.py
+++ __init__.py
@@ -1,68 +1,12 @@
 # by @RimMirK (t.me/RimMirK)
 
-from requests import get, post
-from gzip import compress
+from .main import paste, apaste, LANGUAGES
 
-__version__ = version = '1.0.0'
+__version__ = version = '1.2.0'
 
 __all__ = [
-    'paste'
-    'version'
+    'paste',
+    'apaste',
+    'version',
     'LANGUAGES',
 ]
-
-_api_url = 'https://bytebin.lucko.me/post'
-_pasted_link = 'https://pastes.dev/{}'
-
-
-LANGUAGES = [
-    # text
-    "plain"
-    "log"
-    
-    # config
-    "yaml"
-    "json"
-    "xml"
-    "ini"
-    
-    # code
-    "java"
-    "javascript"
-    "typescript"
-    "python"
-    "kotlin"
-    "scala"
-    "cpp"
-    "csharp"
-    "shell"
-    "ruby"
-    "rust"
-    "sql"
-    "go"
-    
-    # web
-    "html"
-    "css"
-    "scss"
-    "php"
-    "graphql"
-    
-    # misc
-    "dockerfile"
-    "markdown"
-    "proto"
-]
-
-
-def paste(code, language = 'auto'):
-    headers = {
-        'Content-Type': f'text/{language}',
-        'Content-Encoding': 'gzip',
-    }
-
-    gzip_data = compress(code.encode('utf-8'))
-
-    response = post(_api_url, data=gzip_data, headers=headers)
-    
-    return _pasted_link.format(response.json()['key'])
